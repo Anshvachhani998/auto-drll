e39139a83cf0c30fc7b634ce9950a6b92bbf5cb1
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2024-12-20 13:37:30 +0530

    Update cmd.py

--- AnshDelete/cmd.py
+++ AnshDelete/cmd.py
@@ -4,13 +4,15 @@ import pyrogram
 from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated, UserAlreadyParticipant, InviteHashExpired, UsernameNotOccupied
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message 
-from config import API_ID, API_HASH
+from config import API_ID, API_HASH, ERROR_MESSAGE
+
+ALLOWED_GROUP_IDS = [-1002118198358]  # Replace with your group IDs
 
 # start command
 @Client.on_message(filters.command(["start"]))
 async def send_start(client: Client, message: Message):
     buttons = [[
-        InlineKeyboardButton("❣️ Developer", url = "https://t.me/kingvj01")
+        InlineKeyboardButton("❣️ Developer", url = "https://t.me/Legend_BoyCC")
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_message(
@@ -19,3 +21,12 @@ async def send_start(client: Client, message: Message):
         reply_markup=reply_markup, 
         reply_to_message_id=message.id
     )
+
+# Delete messages in specific groups after 5 seconds
+@Client.on_message(filters.chat(ALLOWED_GROUP_IDS) & ~filters.service)
+async def delete_messages(client: Client, message: Message):
+    try:
+        await asyncio.sleep(5)  # Wait for 5 seconds
+        await message.delete()  # Delete the message
+    except Exception as e:
+        print(f"Error deleting message: {e}")
