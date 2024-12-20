8c5f7098712264ebc22c52e480bdf5e3012bf4bd
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2024-12-20 13:09:08 +0530

    Create cmd.py

---
+++ AnshDelete/cmd.py
@@ -0,0 +1,21 @@
+import os
+import asyncio 
+import pyrogram
+from pyrogram import Client, filters, enums
+from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated, UserAlreadyParticipant, InviteHashExpired, UsernameNotOccupied
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message 
+from config import API_ID, API_HASH, ERROR_MESSAGE
+
+# start command
+@Client.on_message(filters.command(["start"]))
+async def send_start(client: Client, message: Message):
+    buttons = [[
+        InlineKeyboardButton("❣️ Developer", url = "https://t.me/kingvj01")
+    ]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await client.send_message(
+        chat_id=message.chat.id, 
+        text=f"<b>👋 Hi {message.from_user.mention}, I am Msg Delete Bot.</b>", 
+        reply_markup=reply_markup, 
+        reply_to_message_id=message.id
+    )
