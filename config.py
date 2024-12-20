658d327ec35eb43ed40f68f445e15a2ce37e94ff
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2024-12-20 14:46:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,3 +11,6 @@ API_HASH = os.environ.get("API_HASH", "cc4dda353688d66c94af69ca48a87bdb")
 
 # Your Owner / Admin Id For Broadcast 
 ADMINS = int(os.environ.get("ADMINS", "6073523936"))
+
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://Ansh089:Ansh089@cluster0.y8tpouc.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DB_NAME", "Anshlogin")
