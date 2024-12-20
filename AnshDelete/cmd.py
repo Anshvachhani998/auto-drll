518821f8db396da5d4b5717c1f9cd9ea28be104e
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2024-12-20 14:52:25 +0530

    Update cmd.py

--- AnshDelete/cmd.py
+++ AnshDelete/cmd.py
@@ -5,12 +5,15 @@ from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated, UserAlreadyParticipant, InviteHashExpired, UsernameNotOccupied
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message 
 from config import API_ID, API_HASH
+from database import db
 
 ALLOWED_GROUP_IDS = [-1002118198358]  # Replace with your group IDs
 
 # start command
 @Client.on_message(filters.command(["start"]))
 async def send_start(client: Client, message: Message):
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id, message.from_user.first_name)
     buttons = [[
         InlineKeyboardButton("❣️ Developer", url = "https://t.me/Legend_BoyCC")
     ]]
