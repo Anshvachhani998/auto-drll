25cb2b4fe744c8eacf837189be63a690d17d2535
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2024-12-20 13:02:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,10 +11,3 @@ API_HASH = os.environ.get("API_HASH", "")
 
 # Your Owner / Admin Id For Broadcast 
 ADMINS = int(os.environ.get("ADMINS", "6073523936"))
-
-# Your Mongodb Database Url
-DB_URI = os.environ.get("DB_URI", "")
-DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
-
-# If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
-ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
