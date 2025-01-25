83266b6abe3fac1791df8896a90db5429fd9ba75
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-01-25 20:07:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            plugins=dict(root="Anshlogin"),
+            plugins=dict(root="AnshDelete"),
             workers=50,
             sleep_threshold=10
         )
